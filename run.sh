825bbaed54dbf75df97e31f869449f72f0d217b7
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-11-01 15:57:40 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 export CUDA_VISIBLE_DEVICES=7
 
-python main_fed.py --dataset mnist --iid --num_channels 1 --model cnn --epochs 1 --gpu 0
+python main_fed.py --dataset mnist --iid --num_channels 1 --model cnn --epochs 50 --gpu 0
 
